3d6d90602fdb02ecf5ebdeb3d3265c215bcfe545
Paolo Galeone <nessuno@nerdz.eu>
2021-12-12 12:12:49 +0100

    day 4, part 2 completed

--- 4/main.py
+++ 4/main.py
@@ -27,15 +27,16 @@ class Bingo(tf.Module):
         rows = tf.reduce_sum(board, axis=0)
         cols = tf.reduce_sum(board, axis=1)
 
-        if tf.logical_or(
+        return tf.logical_or(
             tf.reduce_any(tf.equal(rows, -5)), tf.reduce_any(tf.equal(cols, -5))
-        ):
-            return tf.constant(True)
-        return tf.constant(False)
+        )
 
     # @tf.function
     def __call__(
-        self, extractions: tf.data.Dataset, boards: tf.data.Dataset
+        self,
+        extractions: tf.data.Dataset,
+        boards: tf.data.Dataset,
+        first_winner: tf.Tensor = tf.constant(True),
     ) -> Tuple[tf.Tensor, tf.Tensor]:
         # Convert the datasaet to a tensor  and assign it to the ta
         # use the tensor to get its shape and know the numnber of boards
@@ -53,10 +54,15 @@ class Bingo(tf.Module):
                 board = self._ta.read(idx)
                 board = tf.where(tf.equal(number, board), -1, board)
                 if self.is_winner(board):
-                    self._stop.assign(tf.constant(True))
                     self._winner_board.assign(board)
                     self._last_number.assign(number)
-                    break
+                    if first_winner:
+                        self._stop.assign(tf.constant(True))
+                        break
+                    # When searching for the last winner
+                    # we just invalidate every winning board
+                    # by setting all the values to zero
+                    board = tf.zeros_like(board)
                 self._ta = self._ta.write(idx, board)
         return self._winner_board, self._last_number
 
@@ -88,17 +94,27 @@ def main():
 
     bingo = Bingo()
     winner_board, last_number = bingo(extractions, boards)
-    tf.print("Winner board: ", winner_board)
-    tf.print("Last number: ", last_number)
 
-    # Sum all unmarked numbers
-    unmarked_sum = tf.reduce_sum(
-        tf.gather_nd(winner_board, tf.where(tf.not_equal(winner_board, -1)))
-    )
-    tf.print("Unmarked sum: ", unmarked_sum)
+    def _score(board, number):
+        tf.print("Winner board: ", board)
+        tf.print("Last number: ", number)
 
-    final_score = unmarked_sum * last_number
-    tf.print("Final score: ", final_score)
+        # Sum all unmarked numbers
+        unmarked_sum = tf.reduce_sum(
+            tf.gather_nd(board, tf.where(tf.not_equal(board, -1)))
+        )
+        tf.print("Unmarked sum: ", unmarked_sum)
+
+        final_score = unmarked_sum * number
+        tf.print("Final score: ", final_score)
+
+    _score(winner_board, last_number)
+
+    ## --- Part Two ---
+    # Figure out the last board that will win
+    bingo = Bingo()
+    last_winner_board, last_number = bingo(extractions, boards, tf.constant(False))
+    _score(last_winner_board, last_number)
 
 
 if __name__ == "__main__":
