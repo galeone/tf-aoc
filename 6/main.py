e5b8057e525a2979866202bc9c32958ba3c68b14
Paolo Galeone <nessuno@nerdz.eu>
2021-12-16 20:44:07 +0100

    part 2 raw solution

--- 6/main.py
+++ 6/main.py
@@ -10,7 +10,7 @@ import tensorflow as tf
 
 
 @tf.function
-def count(initial_state: tf.Tensor, days):
+def evolve(initial_state: tf.Tensor, days: tf.Tensor):
     ta = tf.TensorArray(tf.int32, size=tf.size(initial_state), dynamic_size=True)
     ta = ta.unstack(initial_state)
 
@@ -36,6 +36,79 @@ def count(initial_state: tf.Tensor, days):
     return today_state
 
 
+# Returning the state is useless, we are interested only in the # of elements
+# the last state contains.
+# Hence we can try to reason about this number instead of the state.
+# Given an initial state of 1 2 3 4 1 -> tot = 5
+# After a time step, we are in the state 0 1 2 3 0 -> tot 5
+# when we have zeroes, it means will span eights on the next time step.
+# After a time step we are in the state 6 0 1 2 6 8 8
+# If we only keep track of the number of fish in a certain state, we can compress all the information
+# Something like
+# 1 -> 10 #there are 10 fish at status 1. Nothing happens to the lenght
+# 2 -> 1 # there's one fish in status 2. Nothing happens to the lenght
+# k -> v # there's v fish in status k. Depends on on k.
+# If k = 0, it means there are v fish in status 0, hence the lenght (number of fish) will increment by v
+# on the next time step, while the v becomes 0 for k=0 and the number of fishes in status 8 increases by k.a
+@tf.function
+def count(initial_state: tf.Tensor, days: tf.Tensor):
+    hashmap = tf.lookup.experimental.MutableHashTable(
+        tf.int32, tf.int32, tf.constant(0, tf.int32)
+    )
+    keys, _, count = tf.unique_with_counts(initial_state, tf.int32)
+    hashmap.insert(keys, count)
+
+    def _count_elements():
+        return tf.reduce_sum(hashmap.lookup(tf.range(9)))
+
+    for day in tf.range(1, days + 1):
+        yesterday_state = hashmap.lookup(tf.range(9))
+        if tf.greater(yesterday_state[0], 0):
+            tf.print("shift from: ", yesterday_state, summarize=-1)
+            # handled values in keys [0, 5], [7, 8]
+            today_state = tf.tensor_scatter_nd_update(
+                yesterday_state,
+                tf.concat([tf.reshape(tf.range(6), (6, 1)), [[6], [7], [8]]], axis=0),
+                tf.concat(
+                    [
+                        hashmap.lookup(tf.range(1, 7)),
+                        [yesterday_state[7]],
+                        [yesterday_state[8]],
+                        [yesterday_state[0]],
+                    ],
+                    axis=0,
+                ),
+            )
+            tf.print("to: ", today_state, summarize=-1)
+            today_state = tf.tensor_scatter_nd_add(
+                today_state, [[6]], [yesterday_state[0]]
+            )
+            tf.print(
+                "updated position 6 and 8 summing ",
+                yesterday_state[0],
+                "obtaining ",
+                summarize=-1,
+            )
+        else:
+            # shift the the left all the map
+            # put a 0 in 8 position
+            updates = tf.concat(
+                [
+                    tf.unstack(tf.gather(yesterday_state, tf.range(1, 9))),
+                    [tf.constant(0)],
+                ],
+                axis=0,
+            )
+            indices = tf.reshape(tf.range(9), (9, 1))
+            today_state = tf.tensor_scatter_nd_update(yesterday_state, indices, updates)
+            tf.print("complete shift, from ", yesterday_state)
+
+        tf.print("day ", day, " : ", today_state, summarize=-1)
+        hashmap.insert(tf.range(9), today_state)
+        tf.print(_count_elements())
+    return _count_elements()
+
+
 def main():
     """Entrypoint. Suppose the "input" file is in the cwd."""
 
@@ -48,9 +121,12 @@ def main():
         )
     )
 
-    days = tf.constant(256, tf.int32)
-    last_state = count(initial_state, days)
-    tf.print("# fish after ", days, " days: ", tf.size(last_state))
+    # days = tf.constant(80, tf.int32)
+    # last_state = evolve(initial_state, days)
+    # tf.print("# fish after ", days, " days: ", tf.size(last_state))
+
+    days = tf.constant(80, tf.int32)
+    count(initial_state, days)
 
 
 if __name__ == "__main__":
