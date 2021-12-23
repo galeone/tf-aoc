1ac58ca85d0f1ce918737dd14433b67ba3f8dd2e
Paolo Galeone <nessuno@nerdz.eu>
2021-12-23 10:20:51 +0100

    to tf program

--- 9/main.py
+++ 9/main.py
@@ -9,6 +9,66 @@ import sys
 import tensorflow as tf
 
 
+class Finder(tf.Module):
+    def __init__(self, dataset: tf.data.Dataset):
+
+        super().__init__()
+
+        self._count = tf.Variable(0)
+        self._dataset = dataset
+        self._image = tf.convert_to_tensor(list(self._dataset))
+        self._shape = tf.shape(self._image)
+        self._max_value = tf.reduce_max(self._image)
+
+        if tf.not_equal(tf.math.mod(self._shape[0], 3), 0):
+            pad_h = self._shape[0] - (self._shape[0] // 3 + 3)
+        else:
+            pad_h = 0
+        if tf.not_equal(tf.math.mod(self._shape[1], 3), 0):
+            pad_w = self._shape[1] - (self._shape[1] // 3 + 3)
+        else:
+            pad_w = 0
+
+        self._padded_image = tf.pad(
+            self._image,
+            [[0, pad_w], [1, pad_h]],
+            mode="CONSTANT",
+            constant_values=self._max_value,
+        )
+
+        self._padded_shape = tf.shape(self._padded_image)
+
+    @tf.function
+    def risk_levels(self) -> tf.Tensor:
+        neigh_mask = tf.constant([(-1, 0), (0, -1), (1, 0), (0, 1), (0, 0)])
+
+        for y in tf.range(self._padded_shape[0] - 1):
+            for x in tf.range(self._padded_shape[1] - 1):
+                if tf.logical_and(tf.less(y, 1), tf.less(x, 1)):
+                    mask = neigh_mask[2:]
+                elif tf.less(y, 1):
+                    mask = neigh_mask[1:]
+                elif tf.less(x, 1):
+                    mask = tf.concat([[neigh_mask[0]], neigh_mask[2:]], axis=0)
+                else:
+                    mask = neigh_mask
+
+                coords = tf.convert_to_tensor([y, x]) + mask
+                neighborhood = tf.gather_nd(self._padded_image, coords)
+
+                minval = tf.reduce_min(neighborhood)
+                if tf.logical_and(
+                    tf.reduce_any(tf.not_equal(neighborhood, minval)),
+                    tf.equal(minval, self._padded_image[y, x]),
+                ):
+                    self._count.assign_add(1 + self._padded_image[y, x])
+        return self._count
+
+    @tf.function
+    def basins(self) -> tf.Tensor:
+        pass
+
+
 def main():
     """Entrypoint. Suppose the "input" file is in the cwd."""
 
@@ -18,49 +78,8 @@ def main():
         .map(lambda string: tf.strings.to_number(string, out_type=tf.int32))
     )
 
-    image = tf.convert_to_tensor(list(dataset))
-    shape = tf.shape(image)
-    max_value = tf.reduce_max(image)
-
-    if tf.not_equal(tf.math.mod(shape[0], 3), 0):
-        pad_h = shape[0] - (shape[0] // 3 + 3)
-    else:
-        pad_h = 0
-    if tf.not_equal(tf.math.mod(shape[1], 3), 0):
-        pad_w = shape[1] - (shape[1] // 3 + 3)
-    else:
-        pad_w = 0
-
-    padded_image = tf.pad(
-        image, [[0, pad_w], [1, pad_h]], mode="CONSTANT", constant_values=max_value
-    )
-
-    shape = tf.shape(padded_image)
-
-    neigh_mask = tf.constant([(-1, 0), (0, -1), (1, 0), (0, 1), (0, 0)])
-
-    count = tf.Variable(0)
-    for y in tf.range(shape[0] - 1):
-        for x in tf.range(shape[1] - 1):
-            if tf.logical_and(tf.less(y, 1), tf.less(x, 1)):
-                mask = neigh_mask[2:]
-            elif tf.less(y, 1):
-                mask = neigh_mask[1:]
-            elif tf.less(x, 1):
-                mask = tf.concat([[neigh_mask[0]], neigh_mask[2:]], axis=0)
-            else:
-                mask = neigh_mask
-
-            coords = tf.convert_to_tensor([y, x]) + mask
-            neighborhood = tf.gather_nd(padded_image, coords)
-
-            minval = tf.reduce_min(neighborhood)
-            if tf.logical_and(
-                tf.reduce_any(tf.not_equal(neighborhood, minval)),
-                tf.equal(minval, padded_image[y, x]),
-            ):
-                count.assign_add(1 + padded_image[y, x])
-    tf.print(count)
+    finder = Finder(dataset)
+    tf.print("Part one: ", finder.risk_levels())
 
 
 if __name__ == "__main__":
