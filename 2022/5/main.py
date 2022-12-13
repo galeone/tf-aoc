602b96c36eeebb9476dc7d290a47cc2bc697cf34
Paolo Galeone <nessuno@nerdz.eu>
2022-12-13 08:22:20 +0100

    code improval

--- 2022/5/main.py
+++ 2022/5/main.py
@@ -24,7 +24,7 @@ def main(input_path: Path) -> int:
         ta = tf.TensorArray(tf.string, size=0, dynamic_size=True)
         for i in tf.range(stacks):
             substr = tf.strings.strip(tf.strings.substr(line, i * 4, 4))
-            stripped = tf.strings.regex_replace(substr, "\[|\]", "")
+            stripped = tf.strings.regex_replace(substr, r"\[|\]", "")
             ta = ta.write(i, stripped)
 
         return ta.stack()
@@ -46,8 +46,7 @@ def main(input_path: Path) -> int:
     stacks = tf.Variable(tf.zeros((max_stack_size, num_stacks - 1, 1), dtype=tf.string))
 
     def initialize_stacks():
-        tf.print(stacks_tensor, summarize=-1)
-        tf.print(tf.shape(stacks_tensor))
+        stacks.assign(tf.zeros_like(stacks))
 
         # shape = tf.shape(stacks)
         # stacks.assign(
@@ -65,7 +64,6 @@ def main(input_path: Path) -> int:
             tf.range(max_stack_size - tf.shape(stacks_tensor)[0], max_stack_size),
             tf.range(tf.shape(stacks_tensor)[1]),
         )
-
         indices = tf.stack([indices_x, indices_y], axis=-1)
 
         updates = tf.expand_dims(tf.transpose(stacks_tensor), axis=2)
@@ -93,35 +91,32 @@ def main(input_path: Path) -> int:
     def move(line):
         amount = tf.strings.to_number(
             tf.strings.regex_replace(
-                tf.strings.regex_replace(line, "move ", ""), " from \d* to \d*$", ""
+                tf.strings.regex_replace(line, "move ", ""), r" from \d* to \d*$", ""
             ),
             tf.int64,
         )
 
-        source_dest = tf.strings.regex_replace(line, "move \d* from ", "")
+        source_dest = tf.strings.regex_replace(line, r"move \d* from ", "")
         source = (
             tf.strings.to_number(
-                tf.strings.regex_replace(source_dest, " to \d*$", ""), tf.int64
+                tf.strings.regex_replace(source_dest, r" to \d*$", ""), tf.int64
             )
             - 1
         )
 
         dest = (
             tf.strings.to_number(
-                tf.strings.regex_replace(source_dest, "\d* to ", ""), tf.int64
+                tf.strings.regex_replace(source_dest, r"\d* to ", ""), tf.int64
             )
             - 1
         )
 
-        tf.print(line)
         num_element_source = num_elements.lookup([source])[0]
         top = max_stack_size - num_element_source
-        # tf.print("amount: ", amount, " source: ", source, " top: ", top)
 
         read = stacks[top : top + amount, source]
 
         # remove from source
-        # tf.print("removing from source stacks[:amount, source]: ", read)
         indices_x, indices_y = tf.meshgrid(tf.range(top, top + amount), [source])
         indices = tf.reshape(tf.stack([indices_x, indices_y], axis=-1), (-1, 2))
         updates = tf.reshape(tf.repeat("", amount), (-1, 1))
@@ -131,11 +126,7 @@ def main(input_path: Path) -> int:
         )
 
         num_element_dest = num_elements.lookup([dest])[0]
-        # tf.print("num_element in dest: ", num_element_dest)
-        # tf.print(stacks[num_element_dest, :])
-
         top = max_stack_size - num_element_dest - 1
-        # tf.print("top: ", top)
 
         # one a at a time -> reverse
         if one_at_a_time:
@@ -143,9 +134,6 @@ def main(input_path: Path) -> int:
             insert = tf.reshape(insert, (-1, 1))
         else:
             insert = tf.reshape(read, (-1, 1))
-
-        tf.print("inserting in dest: ", insert)
-        tf.print("inserting at pos: ", top - amount + 1, " - ", top + 1)
 
         indices_x, indices_y = tf.meshgrid(tf.range(top - amount + 1, top + 1), [dest])
         indices = tf.reshape(tf.stack([indices_x, indices_y], axis=-1), (-1, 2))
@@ -155,10 +143,8 @@ def main(input_path: Path) -> int:
         )
 
         update_num_elements()
-        # tf.print(tf.squeeze(stacks), summarize=-1)
         return stacks
 
-    """
     tf.print("part 1")
     play = moves_dataset.map(move)
 
@@ -170,7 +156,6 @@ def main(input_path: Path) -> int:
     indices = tf.reshape(tf.stack([indices_y, indices_x], axis=-1), (-1, 2))
 
     tf.print(tf.strings.join(tf.squeeze(tf.gather_nd(stacks, indices)), ""))
-    """
 
     tf.print("part 2")
     initialize_stacks()
