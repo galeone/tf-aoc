2df4aa1873d1ae8448f64d47c895916fb5ea90b6
Paolo Galeone <nessuno@nerdz.eu>
2022-12-26 15:04:38 +0100

    day 9: cleanup

--- 2022/9/main.py
+++ 2022/9/main.py
@@ -1,7 +1,7 @@
 """
 Solution in pure TensorFlow to the puzzle
 
-https://adventofcode.com/2022/day/1
+https://adventofcode.com/2022/day/9
 
 of the Advent of Code 2022.
 """
@@ -13,14 +13,14 @@ import tensorflow as tf
 
 
 def are_neigh(a, b):
-    return tf.math.less_equal(tf.norm((a - b), ord=tf.experimental.numpy.inf), 1)
+    return tf.math.less_equal(tf.norm(a - b, ord=tf.experimental.numpy.inf), 1)
 
 
 # integers to naturals
 def to_natural(z):
     if tf.greater_equal(z, 0):
-        return tf.cast(2 * z, tf.int32)
-    return tf.cast(-2 * z - 1, tf.int32)
+        return tf.cast(2 * z, tf.int64)
+    return tf.cast(-2 * z - 1, tf.int64)
 
 
 def pairing_fn(i, j):
@@ -36,23 +36,22 @@ def main(input_path: Path) -> int:
     dataset = (
         tf.data.TextLineDataset(input_path.as_posix())
         .map(lambda line: tf.strings.split(line, " "))
-        .map(lambda pair: (pair[0], tf.strings.to_number(pair[1], tf.int32)))
+        .map(lambda pair: (pair[0], tf.strings.to_number(pair[1], tf.int64)))
     )
-    head = tf.Variable(tf.zeros((2,), dtype=tf.int32))
+    head = tf.Variable(tf.zeros((2,), dtype=tf.int64))
     tail = tf.Variable(tf.zeros_like(head))
-    pos = tf.lookup.experimental.MutableHashTable(tf.int32, tf.int32, (0, 0, 0))
+    pos = tf.lookup.experimental.MutableHashTable(tf.int64, tf.int64, (-1, 0, 0))
     pos.insert([pairing_fn(0, 0)], [(1, 0, 0)])
 
     def play(direction, amount):
-        tf.print(direction, amount)
 
-        sign = -1
+        sign = tf.constant(-1, tf.int64)
         if tf.logical_or(tf.equal(direction, "U"), tf.equal(direction, "R")):
-            sign = 1
+            sign = tf.constant(1, tf.int64)
 
-        axis = (0, 1)
+        axis = tf.constant((0, 1), tf.int64)
         if tf.logical_or(tf.equal(direction, "R"), tf.equal(direction, "L")):
-            axis = (1, 0)
+            axis = tf.constant((1, 0), tf.int64)
 
         for _ in tf.range(amount):
             head.assign_add(sign * axis)
@@ -65,10 +64,14 @@ def main(input_path: Path) -> int:
 
                 mapped = pairing_fn(tail[0], tail[1])
                 info = pos.lookup([mapped])[0]
+
                 visited, first_coord, second_coord = info[0], info[1], info[2]
-                if tf.equal(visited, 0):
+                if tf.equal(visited, -1):
                     # first time visited
-                    pos.insert([mapped], [(1, tail[0], tail[1])])
+                    pos.insert(
+                        [mapped],
+                        [tf.stack([tf.constant(1, tf.int64), tail[0], tail[1]])],
+                    )
                 elif tf.reduce_all(
                     [
                         tf.equal(visited, 1),
@@ -77,7 +80,10 @@ def main(input_path: Path) -> int:
                     ]
                 ):
                     # mapped to the same number, but different cords (e.g [3, 2] and [2, 3] both mapped to 6)
-                    pos.insert([mapped], [(2, tail[1], tail[0])])
+                    pos.insert(
+                        [mapped],
+                        [tf.stack([tf.constant(2, tf.int64), tail[0], tail[1]])],
+                    )
 
             tf.print("H: ", head)
             tf.print("T: ", tail)
@@ -88,8 +94,7 @@ def main(input_path: Path) -> int:
 
     dataset = dataset.map(play)
 
-    for _ in dataset:
-        pass
+    list(dataset)
 
     # (#visisted, x, y), where # visisted = 1 visited x,y # visisted = 2 visisted x,y and y,x
     tail_positions = pos.export()[1]
@@ -98,31 +103,6 @@ def main(input_path: Path) -> int:
 
     # tf.print(pos.export()[1], summarize=-1)
     tf.print("part 1: ", visited_count)
-
-    """
-    draw = tf.Variable(
-        tf.zeros(
-            (
-                tf.reduce_max(tail_positions[:, 1]) + 1,
-                tf.reduce_max(tail_positions[:, 2]) + 1,
-            ),
-            dtype=tf.int32,
-        )
-    )
-    indices = tail_positions[:, 1:]
-    flipped_indices = tf.reverse_sequence(
-        indices, tf.repeat(2, tf.shape(indices)[0]), seq_axis=1, batch_axis=0
-    )
-
-    # tf.print(indices, summarize=-1)
-    # tf.print(flipped_indices, summarize=-1)
-    update = tf.tensor_scatter_nd_add(
-        draw,
-        flipped_indices,
-        tf.repeat(1, tf.shape(indices)[0]),
-    )
-    tf.print(update, summarize=-1)
-    """
 
     return 0
 
